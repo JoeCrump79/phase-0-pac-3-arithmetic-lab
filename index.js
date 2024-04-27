59275577593fa579b26776e810ded9d8def3c6ea
Joseph Crump <josephcrump79@yahoo.com>
2024-04-26 20:47:18 -0500

    javascriptcalculator

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+} 
+function multiply(c, d) {
+  return c * d;
+} 
+function divide(e, f) {
+  return e / f;
+}
+function increment(e) {
+  return e + 1;
+}
+function decrement(e) {
+  return e-1;
+}
+function makeInt(e) {
+  return parseInt (e,10);
+} 
+function preserveDecimal(n) {
+  return parseFloat (n)
+}
